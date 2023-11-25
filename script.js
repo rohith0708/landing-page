b9f34d95c092bd756afdd91e3b9acb9f96dafd51
Rohith R <rohithr628295@gmail.com>
2023-11-25 15:13:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+gsap.from('.image',{opacity:0, duration:1 , delay:0.2, y:10})
+
+
+
+gsap.from('.navbar',{opacity:0, duration:1 , delay:0.2, y:10})
+
+
+
+gsap.from('.bodyy',{opacity:0, duration:3 , delay:0.2, y:10})
